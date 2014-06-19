2d0e88110e9eca97d15efac449bdc80722ac87df
rbjack <rbjack@gmail.com>
2014-06-19 13:01:12 -0700

    activate GA

--- app/portfolio/application/modules/templates/views/portfolio.php
+++ app/portfolio/application/modules/templates/views/portfolio.php
@@ -74,13 +74,12 @@
         <script src="js/vendor/jquery.bttrlazyloading.min.js"></script>
         <script src="js/script.js"></script>
 
-        <!-- Google Analytics: change UA-XXXXX-X (UA-11924095-1) to be your site's ID. -->
         <script>
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
             })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-            ga('create', 'UA-XXXX-Y', 'auto');ga('send', 'pageview');
+            ga('create', 'UA-11924095-1', 'auto');ga('send', 'pageview');
         </script>
     </body>
 </html>
