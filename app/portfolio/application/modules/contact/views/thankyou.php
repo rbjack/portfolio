77dd4c4487031a2638720cc3da36e7e24eb710e2
rbjack <rbjack@gmail.com>
2014-06-20 08:45:49 -0700

    apply captcha to thankyou page to clear session

--- app/portfolio/application/modules/contact/views/thankyou.php
+++ app/portfolio/application/modules/contact/views/thankyou.php
@@ -1,5 +1,6 @@
 <div class="row block">
                     	<p>Your email has been sent. Thanks for <a href="contact">getting in touch</a>!</p>
+                    	<span class="hidden"><img src="captcha.jpg" /></span>
                     	<br>
                     	<br>
                     </div>
