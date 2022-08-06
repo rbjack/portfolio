5c8d83783ca43fad316c14bcc60527ec30e00372
Jackson <jackson@Jacksons-MacBook-Pro.local>
2022-08-05 17:08:22 -0700

    Add updated GA tracking code;

--- app/portfolio/application/modules/templates/views/portfolio.php
+++ app/portfolio/application/modules/templates/views/portfolio.php
@@ -74,12 +74,7 @@
         <script src="js/vendor/jquery.bttrlazyloading.min.js"></script>
         <script src="js/script.js"></script>
 
-        <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-            ga('create', 'UA-11924095-1', 'auto');ga('send', 'pageview');
-        </script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-11924095-1"></script>
+        <script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-11924095-1');</script>
     </body>
 </html>
